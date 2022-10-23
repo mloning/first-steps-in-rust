ebc1ded7a3e7b36e7b7d95ac3f05ad322ad6bec5
mloning <markus.loning.17@ucl.ac.uk>
2022-10-23 12:46:39 +0100

    minigrep project updated using iterators

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -25,14 +25,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
         // build config from command-line args and env variables
-        let n_args = args.len();
-        if n_args != 3 {
-            panic!("Requires 3 arguments, but found: {n_args}");
-        }
-        let query = args[1].clone();
-        let file = args[2].clone();
+        args.next(); // skipping the first arg which is the name of the program
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not get a query string."),
+        };
+        let file = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not get a file path."),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
@@ -45,28 +49,18 @@ impl Config {
 }
 
 fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
     let query = query.to_lowercase();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,11 +4,7 @@ use std::process;
 
 fn main() {
     // collect command-line arguments
-    let args: Vec<String> = env::args().collect();
-    dbg!(&args);
-
-    // extract arguments
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Parsing command-line arguments failed: {err}");
         process::exit(1);
     });
