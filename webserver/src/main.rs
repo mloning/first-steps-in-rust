444cb3aa7e3206c1f5f326fe44789d4551f5f716
mloning <markus.loning.17@ucl.ac.uk>
2022-10-30 11:29:28 +0000

    webserver project completed 20.1

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -8,18 +9,24 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
-        println!("Connection established!");
+        handle_connection(stream);
     }
+
+    println!("Connection established!");
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    println!("Request: {:#?}", http_request)
+    let (status_line, file) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(file).unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
